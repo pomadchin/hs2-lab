92aba297e474a96549ea9591e65227e7765b8aa7
Grisha Pomadchin <gr.pomadchin@gmail.com>
2013-11-22 23:33:05 +0400

    +correct task descr

--- 17var.hs
+++ 17var.hs
@@ -10,7 +10,9 @@ filterP "" _  = []
 filterP n sn = filter (\(name, surname) -> (name == n) && (surname == sn)) programming
 
 main = do
-putStrLn "DM to Prog List:"
+putStrLn "Для всех студентов, посещающих дискретную математику: "
+putStrLn "если он записан на программирование, сделать так, чтобы на программирование он записан не был, "
+putStrLn "и наоборот (если не записан, то записать): "
 mapM_ putStrLn $ map (\ (pos, (n, sn)) -> show pos ++ "). " ++ n ++ " " ++ sn) 
 	$ zip [1..] $ (students >>= (\st -> filterDm (name st) (surname st))) >>=
 		(\(n, sn) -> if(null $ filterP n sn) then [(n, sn)] else [])
