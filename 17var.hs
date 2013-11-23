e4d4b639f71a1bf62b3e58848476eb55c3edc6b5
Grisha Pomadchin <gr.pomadchin@gmail.com>
2013-11-23 12:44:25 +0400

    optimized

--- 17var.hs
+++ 17var.hs
@@ -1,18 +1,10 @@
 import Lab2
 import Data.List
 
-filterDm _ ""  = []
-filterDm "" _  = []
-filterDm n sn = filter (\(name, surname) -> (name == n) && (surname == sn)) discrete_mathematics
-
-filterP _ ""  = []
-filterP "" _  = []
-filterP n sn = filter (\(name, surname) -> (name == n) && (surname == sn)) programming
-
 main = do
 putStrLn "Для всех студентов, посещающих дискретную математику: "
 putStrLn "если он записан на программирование, сделать так, чтобы на программирование он записан не был, "
 putStrLn "и наоборот (если не записан, то записать): "
 mapM_ putStrLn $ map (\ (pos, (n, sn)) -> show pos ++ "). " ++ n ++ " " ++ sn) 
-	$ zip [1..] $ (students >>= (\st -> filterDm (name st) (surname st))) >>=
-		(\(n, sn) -> if(null $ filterP n sn) then [(n, sn)] else [])
+	$ zip [1..] $ (nub discrete_mathematics) >>=
+		(\(n, sn) -> if(null $ filter (\(name, surname) -> (name == n) && (surname == sn)) programming) then [(n, sn)] else [])
